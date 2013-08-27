1aab915ab6d9aafcbf2b51e41fbccc049a985bca
bvidal <dael99@gmail.com>
2013-08-27 11:25:13 -0400

    Background video playback on Android

---
+++ gen/com/datactil/Teaser/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.datactil.Teaser;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ gen/com/datactil/Teaser/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.datactil.Teaser;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
